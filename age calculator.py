b09c93d8d766fec6fc1a80d23f4e3b43c85dae8d
Favourben19 <157905246+Favourben19@users.noreply.github.com>
2026-01-22 18:13:34 +0100

    Update age calculator.py

--- age calculator.py
+++ age calculator.py
@@ -1,18 +1,5 @@
-first_name = "favour"
-print(   type(first_name) )
-age = 20.5
-print(   type(age))
-age = 20
-print(type (age))
-is_a_boy = True
-print(type(is_a_boy))
-value1 = 12
-value2 = 5
-print(value1 + value2)
-value1 = input("enter the value for value 1")
-value2 = input("enter the value for value 2")
-print(value1 + value2)
 current_year = 2026
 date_of_birth =int (input("enter the year you were born: ") )
 age = current_year - date_of_birth
 print(age)
+
